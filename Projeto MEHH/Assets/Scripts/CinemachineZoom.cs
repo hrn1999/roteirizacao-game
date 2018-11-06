6659d6f50972aa0378587ce12b969214c62f8a41
Henrique Nassi <henrique.npsn@gmail.com>
2018-11-06 21:20:22 -0200

    Fungus e +

---
+++ Projeto MEHH/Assets/Scripts/CinemachineZoom.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CinemachineZoom : MonoBehaviour {
+
+    public CinemachineVirtualCamera vcam;
+
+    public void Zoom(float zoomValor) { 
+        if(vcam.m_Lens.OrthographicSize <= zoomValor){
+            vcam.m_Lens.OrthographicSize += Time.deltaTime * 0.2f;
+            
+        }
+    }
+}
