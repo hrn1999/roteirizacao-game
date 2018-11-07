4f7b389e61f787e0b90ff6ca2e2d606eff6307b0
Heric Felipe de Freitas <35154816+hefefe@users.noreply.github.com>
2018-11-07 19:19:50 -0200

    eqweqw

--- Projeto MEHH/Assets/Scripts/Zoom/ZoomIn.cs
+++ Projeto MEHH/Assets/Scripts/Zoom/ZoomIn.cs
@@ -1,10 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using Cinemachine;
 
-public class CinemachineZoomIn : MonoBehaviour
-{
+public class ZoomIn : MonoBehaviour {
 
     public CinemachineVirtualCamera vcam;
 
--- Projeto MEHH/Assets/Scripts/Zoom/ZoomOut.cs
+++ Projeto MEHH/Assets/Scripts/Zoom/ZoomOut.cs
@@ -1,24 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using Cinemachine;
 
-public class CinemachineZoomOut : MonoBehaviour {
+public class ZoomOut : MonoBehaviour {
 
     public CinemachineVirtualCamera vcam;
 
     public float zoomOut;
-    
+
     //ampliar do normal
     public void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            //ampliar do normal
             if (vcam.m_Lens.OrthographicSize <= zoomOut)
             {
                 vcam.m_Lens.OrthographicSize += Time.deltaTime;
             }
-        }  
+        }
     }
 }
