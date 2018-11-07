5bdc572f7f1f9cde74940f4306919b479637bfb5
Heric Felipe de Freitas <heric_felipe2@hotmail.com>
2018-11-07 10:12:05 -0200

    zoom

--- Projeto MEHH/Assets/Scripts/CinemachineZoom.cs
+++ Projeto MEHH/Assets/Scripts/CinemachineZoom.cs
@@ -7,10 +7,23 @@ public class CinemachineZoom : MonoBehaviour {
 
     public CinemachineVirtualCamera vcam;
 
-    public void Zoom(float zoomValor) { 
-        if(vcam.m_Lens.OrthographicSize <= zoomValor){
-            vcam.m_Lens.OrthographicSize += Time.deltaTime * 0.2f;
-            
+    public float m_zinicial;
+    public float m_zoomax;
+    public float m_zoomin;
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        if (vcam.m_Lens.OrthographicSize <= m_zoomax)
+            vcam.m_Lens.OrthographicSize += Time.deltaTime;
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if(vcam.m_Lens.OrthographicSize >= m_zinicial)
+        {
+            vcam.m_Lens.OrthographicSize -= Time.deltaTime;
         }
     }
+
+
 }
