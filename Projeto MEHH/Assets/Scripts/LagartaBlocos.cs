efa740c4b0424b47464cee18117a3dcd1271c1ea
Heric Felipe de Freitas <35154816+hefefe@users.noreply.github.com>
2018-11-07 12:05:10 -0200

    Lagarta (quase Ok)
    falta de animação da aparição

---
+++ Projeto MEHH/Assets/Scripts/LagartaBlocos.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LagartaBlocos : MonoBehaviour {
+
+    private bool chamar;
+
+    public List<GameObject> plataformas = new List<GameObject>();
+
+    private void Start()
+    {
+        chamar = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        chamar = true;
+        Debug.Log("ENTREI");
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        chamar = false;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (chamar == true && Input.GetKeyDown("x"))
+        { 
+            for (int i = 0; i < plataformas.Count; i++)
+            {
+                plataformas[i].SetActive(true);
+            }
+        }
+    }
+}
