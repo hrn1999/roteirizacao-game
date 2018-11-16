fd02c0fb5901bdf9086ff87481d9b3589ebd667d
Heric Felipe de Freitas <heric_felipe2@hotmail.com>
2018-11-16 15:36:45 -0200

    troca de sprite

---
+++ Dogame/Assets/Scripts/potinho/Encher.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Encher : MonoBehaviour {
+
+    public Sprite[] vazio_cheio;
+    public SpriteRenderer potinho;
+
+    private void Start()
+    {
+        potinho = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetKeyDown(KeyCode.X) && collision.CompareTag("Player"))
+        {
+            potinho.sprite = vazio_cheio[1];
+        }
+    }
+}
