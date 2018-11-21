c501fd2d4ae74fde145e3323e961725042695fec
Heric Felipe de Freitas <heric_felipe2@hotmail.com>
2018-11-21 19:28:01 -0200

    A

---
+++ Dogame/Assets/dialogo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dialogo : MonoBehaviour {
+
+    public GameObject lista[];
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+
+            for(int i = 0; i < lista ; i++)
+            {
+                
+            }
+        }
+    }
+}
