cb45e7b5786c481fbae2de46a2b19d534e3f32cd
Henrique Ribeiro Nassi <41037970+hrn1999@users.noreply.github.com>
2018-11-24 12:08:37 -0200

    Dialog

--- Dogame/Assets/Scripts/DialogManager.cs
+++ Dogame/Assets/Scripts/DialogManager.cs
@@ -5,22 +5,51 @@ using TMPro;
 
 public class DialogManager : MonoBehaviour {
 
-    public GameObject dBox;
     public TextMeshProUGUI textDisplay;
+    public GameObject[] dialogBox;
     public string[] sentences;
-    public Collider2D dog;
+    public float typingSpeed;
     private int index;
 
-    public bool dActive;
+    public Animator dialogBoxAnim;
+    public GameObject continueButton;
+    private AudioSource source;
 
-    public float typingSpeed;
-
-    IEnumerator Type()
+    void Start()
     {
-        foreach (char letter in sentences[index].ToCharArray())
+        StartCoroutine(Type());
+    }
+  
+    IEnumerator Type() {
+        foreach(char letter in sentences[index].ToCharArray())
         {
             textDisplay.text += letter;
             yield return new WaitForSeconds(typingSpeed);
         }
     }
+
+    void Update() {
+            if(textDisplay.text == sentences[index])
+        {
+            continueButton.SetActive(true);
+        }
+    }
+    public void NextSentence() {
+        source.Play();
+        dialogBoxAnim.SetTrigger("Change");
+        continueButton.SetActive(false);
+
+        if (index < sentences.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        }
+        else {
+            textDisplay.text = "";
+            continueButton.SetActive(false);
+        }
+    }
+    
+
 }
