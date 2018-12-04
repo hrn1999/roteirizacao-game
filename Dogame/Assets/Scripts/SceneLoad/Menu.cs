5d03daeb36e8abbf52f49c4e094ad7ade51cb306
Henrique Nassi <henrique.npsn@gmail.com>
2018-12-03 22:15:49 -0200

    001

---
+++ Dogame/Assets/Scripts/SceneLoad/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    public void StartGame(string SceneToLoad)
+    {
+        SceneManager.LoadScene(SceneToLoad);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+}
