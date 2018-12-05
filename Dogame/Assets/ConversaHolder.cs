5411e3cb252026208cfb0c553815cfc1bec88e0c
Heric Felipe de Freitas <35154816+hefefe@users.noreply.github.com>
2018-12-05 14:38:54 -0200

    CONVERSA
    se nao funcionar pega o codigo chamado ConversaHolder e tira o array ( [] ), do gameobject dbox

---
+++ Dogame/Assets/ConversaHolder.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConversaHolder : MonoBehaviour {
+
+    public string dialogue;
+    private ConversaManager cMan;
+
+    public string[] dialogueLines;
+	// Use this for initialization
+	void Start () {
+        cMan = FindObjectOfType<ConversaManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "DogPlayer")
+        {
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                if (!cMan.dialogActive)
+                {
+                    
+                    cMan.dialoaglines = dialogueLines;
+                    cMan.currentLines = 0;
+                    cMan.ShowDialogue();
+                }
+            }
+        }
+    }
+}
