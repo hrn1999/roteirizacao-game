3ddf779cf8de9a8b64cf2e46264b9be729db21a9
Henrique Nassi <henrique.npsn@gmail.com>
2018-12-05 10:05:07 -0200

    a

---
+++ Dogame/Assets/Scripts/EventsMechanics/TriggerMemory.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerMemory : MonoBehaviour {
+
+    public GameObject objectToSee;
+    public SpriteRenderer changeAlpha;
+    public float timeToChange;
+
+    private void Start()
+    {
+        objectToSee.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            objectToSee.SetActive(true);
+        }
+
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            objectToSee.SetActive(false);
+        }
+    }
+}
